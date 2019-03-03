e0c060c8b2ef33bc34aca6cbfc0613e660adb4bc
jahangir091 <jahangir.cse09@gmail.com>
2019-03-03 18:28:05 +0600

    added new model Fact

--- telenor_health/telenor_health/dashboard/models.py
+++ telenor_health/telenor_health/dashboard/models.py
@@ -16,3 +16,15 @@ class FactTable(models.Model):
     title = models.CharField(max_length=300)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
+
+
+class Fact(models.Model):
+    membership_no = models.CharField(max_length=300, null=True, blank=True)
+    first_free_join_date = models.DateTimeField(null=True, blank=True)
+    last_free_subscription_date = models.DateTimeField(null=True, blank=True)
+    first_piad_subscription_date = models.DateTimeField(null=True, blank=True)
+    last_piad_subscription_date = models.DateTimeField(null=True, blank=True)
+    number_of_renew_performed = models.IntegerField(blank=True, null=True)
+    number_of_paid_subscription = models.IntegerField(blank=True, null=True)
+    first_retailer_or_channel = models.CharField(max_length=300, null=True, blank=True)
+    avg_membership_duration = models.FloatField(blank=True, null=True)
